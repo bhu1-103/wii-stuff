be43eaa6eb6d2ffe0013bc11e0df98f9844c60cc
bhu1-103 <103.bhuvan@gmail.com>
2025-05-08 01:41:23 +0530

    Thu May  8 01:41:23 AM IST 2025

--- motion-control/nunchuck.py
+++ motion-control/nunchuck.py
@@ -2,13 +2,10 @@ import subprocess
 from evdev import InputDevice, ecodes
 import sys
 
-# Device path for Nunchuck
-nunchuck_device_path = '/dev/input/event19'  # Replace with your actual Nunchuck device path
+nunchuck_device_path = '/dev/input/event16'
 
-# Threshold for delta change
-DELTA_THRESHOLD = 10
+DELTA_THRESHOLD = 1
 
-# Try to open the Nunchuck input device
 try:
     nunchuck_dev = InputDevice(nunchuck_device_path)
 except FileNotFoundError:
@@ -17,22 +14,17 @@ except FileNotFoundError:
 
 print(f"Listening to: {nunchuck_dev.name} ({nunchuck_device_path})")
 
-# Store last values for delta calculations
 last_values_nunchuck = {}
 
-# Function to move the mouse relatively using ydotool
 def move_mouse_relative(dx, dy):
-    #subprocess.run(['ydotool', 'mousemove', str(dx), str(dy)])
     subprocess.run(['ydotool', 'mousemove', '-x', str(dx), '-y', str(dy)])
 
-# Function to handle events and control the mouse
 def handle_nunchuck_events():
     for event in nunchuck_dev.read_loop():
         if event.type == ecodes.EV_ABS:
             code_name = ecodes.ABS.get(event.code, f"Unknown({event.code})")
             current_value_nunchuck = event.value
 
-            # Delta calculation
             last_value = last_values_nunchuck.get(event.code)
             if last_value is None:
                 last_values_nunchuck[event.code] = current_value_nunchuck
@@ -43,10 +35,9 @@ def handle_nunchuck_events():
                 print(f"Nunchuck - {code_name}: {current_value_nunchuck} (Δ {delta})")
                 last_values_nunchuck[event.code] = current_value_nunchuck
 
-                # Move the mouse relative based on the accelerometer data
                 if event.code == ecodes.ABS_RX:
-                    move_mouse_relative(delta*3, 0)  # Move on X-axis (relative)
+                    move_mouse_relative(delta*2560/512, 0)  # Move on X-axis (relative)
                 elif event.code == ecodes.ABS_RY:
-                    move_mouse_relative(0, delta*3)  # Move on Y-axis (relative)
+                    move_mouse_relative(0, delta*1440/512)  # Move on Y-axis (relative)
 
 handle_nunchuck_events()
